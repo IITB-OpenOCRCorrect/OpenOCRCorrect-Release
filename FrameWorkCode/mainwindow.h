d13ccfcb812e1207fd3a3258cc5d786bed87809d
svtsanoj <svtsanoj@gmail.com>
2020-07-31 16:01:17 +0530

    Minor change

--- FrameWorkCode/mainwindow.h
+++ FrameWorkCode/mainwindow.h
@@ -166,7 +166,7 @@ private slots:
 
     void on_pushButton_3_clicked();
 
-    void on_pushButton_2_clicked();
+    //void on_pushButton_2_clicked();
 
     void on_actionJusitfiedAlign_triggered();
 
